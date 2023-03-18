308888cddfa682276a16e683c0751c55dbdfc9ef
Alissonmaia <alissonmaiadasilva28@gmail.com>
2023-03-18 15:51:27 -0300

    File nano attack site cloner

---
+++ iac_phishing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Criando um phishing no Kali Linux capturar senhas de login"
+ 
+
+echo "iniciando caixa de ferramentas"
+
+setoolkit
+echo "Selecione opção de Engenharia Social"
+1) Social-Engineering Attacks
+
+echo "Selecione vetor de ataque: Website"
+2) Website Attack Vectors
+echo "Método de ataque do coletor de credenciais"
+3) Credential Harvester Attack Method
+echo "Clone Site"
+2) Site Cloner
+echo "Confirme o IP do seu localhost"
+
+echo "Digite url do site que você deseja clonar"
+
+set:webattack> Enter the url to clone:https://twitter.com/
